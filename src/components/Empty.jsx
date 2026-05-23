aba8dfc5239c8d87594c3411e0d4fe6a8bf4db30
thentrsfs <filipstojkov54@gmail.com>
2026-05-23 10:08:13 +0200

    Change position on empty component to absolute

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -9,7 +9,7 @@ const Empty = ({
 		<div
 			className={`${
 				isTabletDropdownOpen && 'ml-[130px]'
-			} fixed top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] w-[90%] md:w-[50%] flex flex-col items-center gap-4 z-10 transition-all duration-400`}>
+			} absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] w-[90%] md:w-[50%] flex flex-col items-center gap-4 z-10 transition-all duration-400`}>
 			<p className='heading-l text-center text-gray-medium '>{paragraph}</p>
 			<Button
 				variant={'primaryL'}
